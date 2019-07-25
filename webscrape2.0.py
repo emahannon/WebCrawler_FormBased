67cfbe094d2693dc12921df6da3464a89185c487
emahannon <georgiannahannon@gmail.com>
2019-07-25 11:15:03 -0400

    final changes for proxy optimization

--- webscrape2.0.py
+++ webscrape2.0.py
@@ -4,15 +4,123 @@
 
 #Requirements: Selenium must be added to PATH.
 
+from __future__ import print_function
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import argparse
 import json
+from selenium.webdriver.chrome.options import Options
+import string
+import zipfile
 
-PROXY = "194.187.216.228:53281" # IP:PORT or HOST:PORT
+PROXY_HOST = "23.94.44.65"
+PROXY_PORT = "10998"
+USERNAME = "netkingz9" 
+PASSWORD = "test123"
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--proxy-server=%s' % PROXY)
+def create_proxyauth_extension(proxy_host, proxy_port,
+                               proxy_username, proxy_password,
+                               scheme='http', plugin_path=None):
+    """Proxy Auth Extension
+
+    args:
+        proxy_host (str): domain or ip address, ie proxy.domain.com
+        proxy_port (int): port
+        proxy_username (str): auth username
+        proxy_password (str): auth password
+    kwargs:
+        scheme (str): proxy scheme, default http
+        plugin_path (str): absolute path of the extension       
+
+    return str -> plugin_path
+    """
+    
+
+    if plugin_path is None:
+        plugin_path = '/tmp/vimm_chrome_proxyauth_plugin.zip'
+
+    manifest_json = """
+    {
+        "version": "1.0.0",
+        "manifest_version": 2,
+        "name": "Chrome Proxy",
+        "permissions": [
+            "proxy",
+            "tabs",
+            "unlimitedStorage",
+            "storage",
+            "<all_urls>",
+            "webRequest",
+            "webRequestBlocking"
+        ],
+        "background": {
+            "scripts": ["background.js"]
+        },
+        "minimum_chrome_version":"22.0.0"
+    }
+    """
+
+    background_js = string.Template(
+    """
+    var config = {
+            mode: "fixed_servers",
+            rules: {
+              singleProxy: {
+                scheme: "${scheme}",
+                host: "${host}",
+                port: parseInt(${port})
+              },
+              bypassList: ["foobar.com"]
+            }
+          };
+
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+    function callbackFn(details) {
+        return {
+            authCredentials: {
+                username: "${username}",
+                password: "${password}"
+            }
+        };
+    }
+
+    chrome.webRequest.onAuthRequired.addListener(
+                callbackFn,
+                {urls: ["<all_urls>"]},
+                ['blocking']
+    );
+    """
+    ).substitute(
+        host=proxy_host,
+        port=proxy_port,
+        username=proxy_username,
+        password=proxy_password,
+        scheme=scheme,
+    )
+    with zipfile.ZipFile(plugin_path, 'w') as zp:
+        zp.writestr("manifest.json", manifest_json)
+        zp.writestr("background.js", background_js)
+
+    return plugin_path
+
+proxyauth_plugin_path = create_proxyauth_extension(
+    proxy_host="23.94.44.65",
+    proxy_port=10998,
+    proxy_username="netkingz9",
+    proxy_password="test123"
+)
+
+
+co = Options()
+co.add_argument("--start-maximized")
+co.add_extension(proxyauth_plugin_path)
+
+
+
+
+
 
 
 #ACTUAL SCRAPING
@@ -30,7 +138,7 @@ args = parser.parse_args()
 vin_num = str(args.string)
 
 
-browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(options=co)
 browser.get('https://www.iseecars.com/vin')
 #find form
 elem = browser.find_element_by_id("vin-field") #VIN by name #vin-field by id
